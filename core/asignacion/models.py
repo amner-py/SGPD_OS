9d96083ab5f9897bb17b0805dbdd4131cdfc57dd
Omar Perez <aomarperez17@gmail.com>
2022-10-13 10:42:01 -0600

    Se agrego el modelo para asignacion, y se hicieron cambios respectivos en el sql

--- core/asignacion/models.py
+++ core/asignacion/models.py
@@ -1,3 +1,18 @@
+import re
 from django.db import models
+from delegacion.models import Delegacion
+from lugar.models import Lugar
 
-# Create your models here.
+
+class Asignacion(models.Model):
+    delegacion=models.ForeignKey(Delegacion,db_name='DELEGACION_ID',primary_key=True,on_delete=models.CASCADE)
+    lugar=models.ForeignKey(Lugar,db_column='LUGAR_ID',primary_key=True,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table='ASIGNACION'
+        verbose_name='ASIGNACION'
+        verbose_name_plural='ASIGNACIONES'
+
+
+    def __str__(self):
+        return f'{self.delegacion} - {self.lugar}'
